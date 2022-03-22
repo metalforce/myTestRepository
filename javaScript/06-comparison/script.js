7b8930f12c9a67b0c68ae028236a81bb5f506704
Lutsevich Aleksandr <aleksandr.lutsevich@simpleone.ru>
2022-03-22 11:00:19 +0300

    06-comparison

---
+++ javaScript/06-comparison/script.js
@@ -0,0 +1,9 @@
+// Каким будет результат этих выражений?
+
+alert(5 > 4); // true
+alert("ананас" > "яблоко"); // false
+alert("2" > "12"); // true
+alert(undefined == null); // true
+alert(undefined === null); // false
+alert(null == "\n0\n"); // false
+alert(null === +"\n0\n"); // false

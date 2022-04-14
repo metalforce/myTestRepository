ba9ed81154233c1aae6e2dccee85778f68c0ec1a
Lutsevich Aleksandr <aleksandr.lutsevich@simpleone.ru>
2022-04-14 16:20:29 +0300

    mumbling-fixes-1.0

--- javaScript/00-tasks/02-week/mumbling/mumbling.js
+++ javaScript/00-tasks/02-week/mumbling/mumbling.js
@@ -2,7 +2,7 @@
 // https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/solutions/javascript/me/best_practice - решение
 
 function accum(s) {
-    return s.split('').map((v, i) => (v.toUpperCase() + v.toLowerCase().repeat(i))).join('-');
+    return s.split('').map((v, i) => v.toUpperCase() + v.toLowerCase().repeat(i)).join('-');
 }
 
 console.log(accum("AsFnGuTgx"));
